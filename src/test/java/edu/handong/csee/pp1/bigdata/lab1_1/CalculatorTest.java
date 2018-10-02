2c3d44b6f6d365bcb173298618e304d4346cb838
heeseok <dldydldy@naver.com>
2018-10-02 21:05:57 +0900

    Correct expected value 4 to 5 in testMain method.

--- src/test/java/edu/handong/csee/pp1/bigdata/lab1_1/CalculatorTest.java
+++ src/test/java/edu/handong/csee/pp1/bigdata/lab1_1/CalculatorTest.java
@@ -11,7 +11,7 @@ public class CalculatorTest {
 		String[] args = {"2", "3"};
 		Calculator.main(args);
 		// assertEquals compares the result of an method call with the expected result.
-		assertEquals(Calculator.getCount(),4);
+		assertEquals(Calculator.getCount(),5);
 	}
 	
 	@Test
@@ -28,7 +28,8 @@ public class CalculatorTest {
 	
 	@Test
 	public void testMod() {
-		// TODO Complete this test case
+		Calculator myCalc = new Calculator();
+		assertEquals(myCalc.mod(5, 3),2);
 	}
 
 	@Test
